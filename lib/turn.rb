658e507a6877d2643c4665b3fbb258823611e287
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 17:07:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
+  input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index, "X")
     display_board(board)
