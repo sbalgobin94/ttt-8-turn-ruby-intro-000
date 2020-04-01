ae6e43227199c5f70d012c7d9fb8c9200e39a880
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 17:06:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index, value)
+    move(board, index, "X")
     display_board(board)
   else
     turn(board)
